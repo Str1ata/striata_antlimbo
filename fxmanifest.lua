fbd91e91723d72e3247343c2438b43fc128bbb15
Str1ata <vitor_btista@hotmail.com>
2023-05-15 13:12:58 -0300

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Striata <striatashop@hotmail.com>'
+description 'striata anti limbo (Striata shop)'
+version '1.0'
+
+lua54 'yes'
+
+client_scripts {
+	"script_config.lua",
+	"client.lua"
+}
+
+server_scripts {
+	"script_config.lua",
+	"server.lua"
+}
